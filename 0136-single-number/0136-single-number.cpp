b1954a2c7ca3c738e29eea918a9b6b54d563babc
Ronak Kumar <156431627+ronak-kumar-sing@users.noreply.github.com>
2026-07-08 22:34:36 +0530

    Time: 0 ms (100%), Space: 20.6 MB (83.71%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int val = 0;
+        for(int i : nums){
+            val = val ^ i;  
+        }
+
+        return val;
+    }
+};
