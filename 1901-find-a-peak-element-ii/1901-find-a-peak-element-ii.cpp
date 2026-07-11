f762aa09e89da6c21917d77967b79cea144c3b06
Ronak Kumar <156431627+ronak-kumar-sing@users.noreply.github.com>
2026-07-11 11:29:22 +0530

    Time: 0 ms (100%), Space: 50.2 MB (67.34%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
-        int  n = mat.size();
+        int n = mat.size();
         int m = mat[0].size();
 
-
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if(i != 0 && mat[i - 1][j] > mat[i][j]) continue;
-                if(i != n-1 && mat[i+1][j] > mat[i][j]) continue;
-                if(j != 0 && mat[i][j-1] > mat[i][j]) continue;
-                if(j != m-1 && mat[i][j+1] > mat[i][j]) continue;
-                return {i,j};
+        int i = 0;
+        int j = 0;
+        while (i < n && j < m) {
+            // pair<int , pair<int,int>> maxValIdx = {-1,{-1,-1}};
+            if (i != 0 && mat[i - 1][j] > mat[i][j]) {
+                i--;
+                continue;
             }
+            if (i != n - 1 && mat[i + 1][j] > mat[i][j]) {
+                i++;
+                continue;
+            }
+            if (j != 0 && mat[i][j - 1] > mat[i][j]) {
+                j--;
+                continue;
+            }
+            if (j != m - 1 && mat[i][j + 1] > mat[i][j]) {
+                j++;
+                continue;
+            }
+            return {i, j};
         }
 
-        return {-1,-1};
+        return {-1, -1};
     }
 };
