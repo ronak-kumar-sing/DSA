fcf114493dafcdfa3167f478453c1f3c04eba5a7
Ronak Kumar <156431627+ronak-kumar-sing@users.noreply.github.com>
2026-07-11 18:45:36 +0530

    Time: 0 ms (100%), Space: 9.3 MB (82.19%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char, char> mp;
-        unordered_map<char, char> mp1;
-
+        vector<int> map1(256 , -1);
+        vector<int> map2(256 , -1);
         int n = s.size();
-        for (int i = 0; i < n; i++) {
-            if (mp.empty() || (!mp.count(s[i]) && !mp1.count(t[i]))) {
-                mp[s[i]] = t[i];
-                mp1[t[i]] = s[i];
-            } else if (mp[s[i]] != t[i] && mp1[t[i]] != s[i])
-                return false;
 
-            cout << mp1[t[i]] << "==" << s[i]<<endl;
-            cout << mp[s[i]] << "==" << t[i]<<endl;
+        for(int i = 0; i < n ; i++){
+            if(map1[s[i]] != -1 && map1[s[i]] != t[i]) return false;
+            else if(map2[t[i]] != -1 && map2[t[i]] != s[i]) return false;
+            else{
+                map1[s[i]] = t[i];
+                map2[t[i]] = s[i];
+            }
         }
+
 
         return true;
     }
